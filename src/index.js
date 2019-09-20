fe0dbed183ded01888b34746f4ca0313e691de75
张龙 <zhanglong@zhanglongdeMacBook-Pro.local>
2019-09-20 17:40:11 +0800

    :tada: init - react-electron

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/index.scss';
+import Router from './router/index.jsx'
 import * as serviceWorker from './serviceWorker';
+
+const App = Router()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
